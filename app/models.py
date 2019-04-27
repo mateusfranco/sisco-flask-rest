2d5582d03f72d89460fbf687bd68de401343551d
newtonius <mateusfrancofs@hotmail.com>
2019-04-26 21:01:42 -0300

    parte de api documentos

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class Document(db.Model):
     __tablename__ = 'document'
     
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256))
+    name = db.Column(db.String(256), unique=True)
     category = db.Column(db.String(128))
     user = db.Column(db.Integer)
     avaliado = db.Column(db.Boolean)
--- resources/Document.py
+++ resources/Document.py
@@ -6,13 +6,55 @@ from datetime import datetime
 from app.models import Document
 from app import db, redis
 
+def verify_token(json):
+    json_data = request.get_json()
+    email = json_data["email"]
+    token_verify = json_data["token"]
+    token = redis.hget(email, "field1")
+    return(token == token_verify)
+
+def data_json():
+    json_data = request.get_json(force=True)
+    if(not json_data):
+        return jsonify({'message': 'No input data provided'}), 400   
+    return json_data    
 
 class Document_CRUD(Resource):
+    
+    
+
     def get(self):
         pass
+
     def post(self):
-        pass
+        json_data = data_json()
+        if(verify_token(json_data)):
+            return jsonify({'message': 'token wrong'}), 400
+        document = Document(name= json_data["name"],
+                                category= json_data["category"],
+                                user= json_data["user_id"],
+                                avaliado= False,
+                                directory = json_data["directory"])
+        db.session.add(document)
+        db.session.commit()
+        return 200
+
     def put(self):
-        pass
+        json_data = data_json()
+        if(verify_token(json_data)):
+            return jsonify({'message': 'token wrong'}), 400
+        document = Document.query.filter_by(name=json_data["name"])
+        document.name = json_data["name"]
+        document.category = json_data["category"]
+        db.session.add(document)
+        db.session.commit()
+        return 200
+
     def delete(self):
-        pass
+        json_data = data_json()
+        if(verify_token(json_data)):
+            return jsonify({'message': 'token wrong'}), 400
+        document = Document.query.filter_by(name=json_data["name"])
+        db.session.delete(document)
+        db.session.commit()
+        return 200
